eb9463e9b7990c1c0c1d13ab9c3f294b3336dddc
jhs512 <jangka512@gmail.com>
2023-11-30 13:22:35 +0900

    setting 2

--- src/test/java/com/ll/sb231130/Sb231130ApplicationTests.java
+++ src/test/java/com/ll/sb231130/Sb231130ApplicationTests.java
@@ -2,8 +2,10 @@ package com.ll.sb231130;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class Sb231130ApplicationTests {
 
     @Test
