f38d61eae1fd79f1e4ca4fb08b685091c799f411
jhs512 <jangka512@gmail.com>
2023-12-07 11:31:06 +0900

    jwt

---
+++ src/test/java/com/ll/sb231130/global/util/jwt/JwtUtilTest.java
@@ -0,0 +1,21 @@
+package com.ll.sb231130.global.util.jwt;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class JwtUtilTest {
+    @Test
+    @DisplayName("t1")
+    void t1() {
+        Map<String, String> data = Map.of("name", "홍길동", "age", "22");
+        String jwtToken = JwtUtil.encode(data);
+
+        System.out.println(jwtToken);
+
+        assertThat(jwtToken).isNotNull();
+    }
+}
